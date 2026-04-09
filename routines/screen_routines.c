2c4f86ce72e51b56a22205413044f29fbbdac677
Matt Swart <Matt.Swart+cba@cba.com.au>
2026-04-09 15:43:47 +1000

    Remove unnecessary includes in screen_routines.c for cleaner code

--- routines/screen_routines.c
+++ routines/screen_routines.c
@@ -1,11 +1,7 @@
 #include "screen_routines.h"
 #include "copper_routines.h"
-#include <proto/exec.h>
-#include <exec/execbase.h>
-#include <hardware/custom.h>
 #include <proto/graphics.h>
 #include <graphics/gfx.h>
-#include "support/gcc8_c_support.h"
 
 extern struct ExecBase *SysBase;
 extern volatile APTR VBR;
